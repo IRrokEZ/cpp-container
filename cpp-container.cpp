eb15ca8ce7c0480dfd9abe6d8e1520097476f2f7
vansergh <vansergh@yandex.ru>
2024-06-27 13:59:04 +0400

    first commit

---
+++ cpp-container.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <string>
+#include "cpp-container.hpp"
+
+using namespace std;
+
+template <typename T>
+void PrintContainer(Container<T>& container) {
+	size_t size{ container.Size() };
+	cout << "Size: "s << size << endl;
+	cout << "--------------------------------"s << endl;
+	for (size_t index{ 0 }; index < size; ++index) {
+		cout << '<' << (index+1) << "> "s << container[index] << endl;
+	}
+	cout << "--------------------------------"s << endl;
+}
+
+int main()
+{
+
+ 	Container<string> s1{ "hello","world" };
+	cout << "Container S1: " << endl;
+	PrintContainer(s1);
+	Container<string> s2;
+	s1[0] = "Hi";
+	cout << "Container S1: " << endl;
+	PrintContainer(s1);
+	cout << "Container S2: " << endl;
+	PrintContainer(s2);
+	Container<string> s3 = { "one","two","three" };
+	cout << "Container S3: " << endl;
+	PrintContainer(s3);
+	s2 = s3;
+	cout << "Container S2: " << endl;
+	PrintContainer(s2);
+	s2.Resize(1);
+	cout << "Container S2: " << endl;
+	PrintContainer(s2);	
+	s3 = s1;
+	cout << "Container S3: " << endl;
+	PrintContainer(s3);
+	s3 = {};
+	cout << "Container S3: " << endl;
+	PrintContainer(s3);	
+
+	cout << "***********************************************" << endl;
+	
+	Container<int> a{ 1,2,3,4 };
+	Container<int> b;
+	Container<int> c = { 20,30,40 };
+	Container d({ 1,2,3,4 });
+	Container e(c);
+	c[0] = 100;
+	b = a;
+	a[0] = 1000;
+
+	cout << "Container B: " << endl;
+	PrintContainer(b);
+	cout << "Container A: " << endl;
+	PrintContainer(a);
+	cout << "Container D: " << endl;
+	PrintContainer(d);
+	cout << "Container E: " << endl;
+	PrintContainer(e);
+
+ 
+	return 0;
+}
---
+++ cpp-container.hpp
@@ -0,0 +1,117 @@
+﻿#pragma once
+
+template <typename T>
+class Container {
+public:
+	Container() {
+		Initialize();
+	}
+	Container(const Container& from) {
+		Initialize(from.size_);
+		*this = from;
+	}
+	Container(const size_t size, const T& with = {}) {
+		Initialize(size);
+		for (size_t index = 0; index < size; ++index) {
+			data_[index] = with;
+		}
+	}
+	Container(const std::initializer_list<T>& list) {
+		Initialize(list.size());
+		size_t index{ 0 };
+		for (const auto& item : list) {
+			data_[index] = item;
+			++index;
+		}
+	}
+	~Container() {
+		Clear();
+	}
+	Container& operator=(const Container& right) {
+		Clear();
+		Initialize(right.size_);
+		CopyData(data_, right.data_, right.size_);
+		resize_count_ = right.resize_count_;
+		capacity_ = right.capacity_;
+		return *this;
+	}
+	T& operator[](const size_t index) {
+		if (index < size_) {
+			return data_[index];
+		}
+		else {
+			throw std::out_of_range("Out of range");
+		}
+	}
+	size_t Size() {
+		return size_;
+	}
+	void PushBack(const T& item) {
+		Resize(size_ + 1);
+		data_[size_] = item;
+	}
+	void Resize(const size_t new_size) {
+		if (!UpdateCapacity(new_size)) {
+			size_ = new_size;
+			return;
+		}
+		T* ptr = new T[capacity_];
+		if (ptr == nullptr) {
+			throw std::runtime_error("Memory not allocated");
+		}
+		CopyData(ptr, data_, (new_size > size_ ? size_ : new_size));
+
+		delete[] data_;
+		data_ = ptr;
+		size_ = new_size;
+	}
+	void Clear() {
+		delete[] data_;
+		data_ = nullptr;
+		size_ = 0;
+		resize_count_ = DEFAULT_RESIZE_COUNT_;
+		capacity_ = DEFAULT_CAPACITY_;
+	}
+private:
+	const unsigned short RESIZE_MULT_{ 2 };
+	const unsigned int DEFAULT_RESIZE_COUNT_{ 10 };
+	const unsigned int DEFAULT_CAPACITY_{ 10 };
+
+	T* data_{ nullptr };
+	size_t size_{ 0 };
+	size_t capacity_{ DEFAULT_CAPACITY_ };
+	unsigned int resize_count_{ DEFAULT_RESIZE_COUNT_ };
+	
+
+	void Initialize(const size_t init_size = 0) {
+		if (data_ != nullptr) {
+			return;
+		}
+		UpdateCapacity(init_size);
+		T* ptr = new T[capacity_];
+		if (ptr == nullptr) {
+			throw std::runtime_error("Memory not allocated");
+		}
+		size_ = init_size;
+		data_ = ptr;
+	}
+	void CopyData(T* destination, const T* source, size_t size) {
+		for (size_t index = 0; index < size; ++index) {
+			destination[index] = source[index];
+		}
+	}
+	bool UpdateCapacity(const size_t feature_size) {
+		if (feature_size > capacity_) {
+			capacity_ = feature_size + resize_count_;
+			resize_count_ *= RESIZE_MULT_;
+			return true;
+		}
+		else if ((feature_size + resize_count_) < capacity_) {
+			capacity_ = feature_size + DEFAULT_RESIZE_COUNT_;
+			resize_count_ = DEFAULT_RESIZE_COUNT_;
+			return true;			
+		}
+		return false;
+	}
+};
+
